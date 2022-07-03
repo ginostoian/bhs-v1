63ba29d2f334c83aca616f8d5bd78466fc5847b0
Gino Stoian <ginostoian@gmail.com>
2022-07-03 20:43:58 +0300

    add arrow controls for testimonials

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,5 +1,7 @@
 const wrapper = document.querySelector('.wrapper');
 const indicators = [...document.querySelectorAll('.indicators button')];
+const leftControl = document.querySelector('.arrow-container-left')
+const rightControl = document.querySelector('.arrow-container-right')
 
 let currentTestimonial = 0; // Default 0
 
@@ -10,4 +12,22 @@ indicators.forEach((item, i) => {
         item.classList.add('active');
         currentTestimonial = i;
     })
+})
+
+leftControl.addEventListener('click', () => {
+    if (currentTestimonial > 0) {
+        indicators[currentTestimonial].classList.remove('active');
+        currentTestimonial--
+        wrapper.style.marginLeft = `-${100 * currentTestimonial}%`
+        indicators[currentTestimonial].classList.add('active')
+    }
+})
+
+rightControl.addEventListener('click', () => {
+    if (currentTestimonial < indicators.length - 1) {
+        indicators[currentTestimonial].classList.remove('active');
+        currentTestimonial++
+        wrapper.style.marginLeft = `-${100 * currentTestimonial}%`
+        indicators[currentTestimonial].classList.add('active')
+    }
 })
