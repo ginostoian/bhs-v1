280937b7acce40deca203803d30a36a8d6bb3566
Gino Stoian <ginostoian@gmail.com>
2022-07-03 21:45:30 +0300

    add dropdown functionality for nav on mobile

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,7 +1,15 @@
 const dropdownBtn = document.querySelector('.dropdown-item')
 const dropdownMenu = document.querySelector('.dropdown-menu')
 
+const dropdownBtnMobile = document.querySelector('.dropdown-item-mobile')
+const dropdownMenuMobile = document.querySelector('.dropdown-menu-mobile')
+
 dropdownBtn.addEventListener('click', (e) => {
     e.preventDefault()
     dropdownMenu.classList.toggle('dropdown-menu-visible')
+})
+
+dropdownBtnMobile.addEventListener('click', (e) => {
+    e.preventDefault()
+    dropdownMenuMobile.classList.toggle('dropdown-menu-visible-mobile')
 })
