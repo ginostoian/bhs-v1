1c17dfa65dfeca3b4c4bf5a6aa15ff397648ded1
Gino Stoian <ginostoian@gmail.com>
2022-07-10 00:01:36 +0300

    fix issue with dropdown dissapearing before being able to click

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -4,9 +4,18 @@ const dropdownMenu = document.querySelector('.dropdown-menu')
 const dropdownBtnMobile = document.querySelector('.dropdown-item-mobile')
 const dropdownMenuMobile = document.querySelector('.dropdown-menu-mobile')
 
+const toggleNav = () => {
+    dropdownMenu.classList.toggle('dropdown-menu-visible')
+}
+
 dropdownBtn.addEventListener('mouseenter', (e) => {
     e.preventDefault()
-    dropdownMenu.classList.toggle('dropdown-menu-visible')
+    toggleNav()
+})
+
+dropdownMenu.addEventListener('mouseleave', (e) => {
+    e.preventDefault()
+    toggleNav()
 })
 
 dropdownBtnMobile.addEventListener('click', (e) => {
